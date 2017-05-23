a9c0dd0dfe7628c6b15defb0f05930a0a54ef0b4
Soham Banerjee <soham2008xyz@gmail.com>
2017-05-23 10:37:23 +0530

    Add API endpoint for show details

--- start.py
+++ start.py
@@ -7,6 +7,11 @@ import urllib.request
 
 # Base URLs
 SHOWS_BASE = 'https://putlocker.fit/a-z-shows/'
+SHOW_BASE = 'https://putlocker.fit/show/'
+
+# Common config
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+headers = { 'User-Agent' : user_agent }
 
 # Bootstrap Flask server
 app = Flask(__name__)
@@ -17,8 +22,6 @@ def hello_world():
 
 @app.route('/show/')
 def get_shows():
-	user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-	headers = { 'User-Agent' : user_agent }
 	request = urllib.request.Request(SHOWS_BASE, None, headers)
 	with urllib.request.urlopen(request) as response:
 		soup = BeautifulSoup(response.read().decode('utf-8'), 'lxml')
@@ -37,4 +40,30 @@ def get_shows():
 
 @app.route('/show/<show>')
 def get_show(show):
-	return show
+	request = urllib.request.Request(SHOW_BASE + show + '/', None, headers)
+	with urllib.request.urlopen(request) as response:
+		soup = BeautifulSoup(response.read().decode('utf-8'), 'lxml')
+		seasons = {}
+		for season in soup.select('h2 > a'):
+			title = season.string
+			episodes = []
+			for episode in season.parent.find_next_sibling('table').find_all('a'):
+				episode_data = {}
+				title_parts = episode['title'].split(' - ')
+				episode_data['title'] = episode.string + ((' - ' + title_parts[len(title_parts) - 1]) if title_parts[len(title_parts) - 1] else '' )
+				url_parts = episode['href'].split('/')
+				episode_data['season'] = url_parts[len(url_parts) - 3].split('-')[1]
+				episode_data['episode'] = url_parts[len(url_parts) - 2].split('-')[1]
+				episode_data['url'] = url_for('get_episode', show = show, season = url_parts[len(url_parts) - 3], episode = url_parts[len(url_parts) - 2])
+				episodes.append(episode_data)
+			seasons[title] = episodes
+		show = {}
+		nav_crumbs = soup.select('.breadcrumb-item')
+		show['name'] = nav_crumbs[len(nav_crumbs) - 1].string
+		show['image'] = soup.select('.thumb.pull-left > img')[0]['src']
+		show['seasons'] = seasons
+	return json.dumps(show)
+
+@app.route('/show/<show>/<season>/<episode>')
+def get_episode(show, season, episode):
+	return show + ' - ' + season + ' - ' + episode
