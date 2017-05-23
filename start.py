13d8644706c0b6d4249312e55621de569cd6c4ef
Soham Banerjee <soham2008xyz@gmail.com>
2017-05-23 15:52:34 +0530

    Ad API endpoint for episode details

--- start.py
+++ start.py
@@ -4,11 +4,13 @@ from flask import Flask, url_for
 import json
 import lxml
 import re
+import requests
 import urllib.request
 
 # Base URLs
 SHOWS_BASE = 'https://putlocker.fit/a-z-shows/'
 SHOW_BASE = 'https://putlocker.fit/show/'
+API_BASE = 'https://putlocker.fit/wp-admin/admin-ajax.php'
 
 # Common config
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
@@ -70,4 +72,32 @@ def get_show(show):
 @app.route('/show/<show>/<season>/<episode>')
 @crossdomain(origin = '*')
 def get_episode(show, season, episode):
-	return show + ' - ' + season + ' - ' + episode
+	request = urllib.request.Request(SHOW_BASE + show + '/' + season + '/' + episode + '/', None, headers)
+	with urllib.request.urlopen(request) as response:
+		soup = BeautifulSoup(response.read().decode('utf-8'), 'lxml')
+		jsondata = json.loads(soup.find(inline_script).string.split(' = ')[1].split(';')[0])
+		postdata = {
+			'action': 'get_src',
+			'data': jsondata['id_token'],
+			'post_id': jsondata['postid'],
+			'e': jsondata['eid'],
+			'm': jsondata['mid']
+		}
+		r = requests.post(url = API_BASE, data = postdata)
+		postdata = {
+			'action': 'get_playlist',
+			'e': jsondata['eid'],
+			'x': r.text.split(', ')[0].split('\'')[1],
+			'y': r.text.split(', ')[1].split('\'')[1]
+		}
+		r2 = requests.post(url = API_BASE, data = postdata)
+		videodata = json.loads(r2.text)
+		video = {}
+		video['image'] = jsondata['poster']
+		video['tracks'] = videodata['tracks']
+		video['sources'] = videodata['sources']
+	return json.dumps(video)
+
+# Utility functions
+def inline_script(tag):
+	return tag.name == 'script' and not tag.has_attr('src')
